12dd5ada7a7b32b0e62370e720c09f99743889a5
Cristi Cretu <cristicretu38@gmail.com>
2021-05-23 10:51:43 +0300

    Add little update

---
+++ Pokedex/Pokedex/ PokemonViewController.swift
@@ -0,0 +1,8 @@
+//
+//   PokemonViewController.swift
+//  Pokedex
+//
+//  Created by Cristi Cretu on 23.05.2021.
+//
+
+import Foundation
---
+++ Pokedex/Pokedex/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Cristi Cretu on 23.05.2021.
+//
+
+import Foundation
+
+struct Pokemon {
+    let name: String
+    let number: Int
+}
