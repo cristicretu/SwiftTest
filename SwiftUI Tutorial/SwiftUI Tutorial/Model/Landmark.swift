eaf01f8e9f84454beacffb9ac2b24271db9c02f2
Cristi Cretu <cristicretu38@gmail.com>
2021-06-19 19:58:51 +0300

    m

---
+++ SwiftUI Tutorial/SwiftUI Tutorial/Model/Landmark.swift
@@ -0,0 +1,36 @@
+//
+//  Landmark.swift
+//  SwiftUI Tutorial
+//
+//  Created by Cristi Cretu on 19.06.2021.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var ImageName: String
+    var image: Image {
+        Image(ImageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+
+    }
+}
---
+++ SwiftUI Tutorial/SwiftUI Tutorial/Model/ModelData.swift
@@ -0,0 +1,30 @@
+//
+//  ModelData.swift
+//  SwiftUI Tutorial
+//
+//  Created by Cristi Cretu on 19.06.2021.
+//
+
+import Foundation
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't loat \(filename) from main bundle: \n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
