f12cb99fdb5345030bbc1a48ee13423814deddce
Cristi Cretu <cristicretu38@gmail.com>
2021-06-20 12:18:58 +0300

    Finish What's Next UI

--- beta_unselfold/beta_unselfold/ContentView.swift
+++ beta_unselfold/beta_unselfold/ContentView.swift
@@ -81,35 +81,71 @@ import CoreData
 
 struct ContentView: View {
     var body: some View {
-        ZStack(alignment: .leading) {
-            RoundedRectangle(cornerRadius: 25)
-                .strokeBorder(Color.gray.opacity(0.6), lineWidth: 1)
-                .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).shadow(color: Color.black.opacity(0.1), radius: 5.0))
-                .frame(width: 150, height: 150)
-            Text("Go for a run")
-                .font(.system(size: 20))
-                .fontWeight(.bold)
-                .padding(.all, 10.0)
-                .frame(width: 85.0, height: 85.0)
-            Text("🏃🏻‍♂️")
-                .font(.system(size: 60))
-                .padding(.leading, 85.0)
-//            Button {
-//                print("Button pressed")
-//            } label: {
-//                RoundedRectangle(cornerRadius: 25)
-//                    .strokeBorder(Color.gray.opacity(0.6), lineWidth: 1)
-//                    .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).shadow(color: Color.black.opacity(0.1), radius: 5.0))
-//                    .frame(width: 20, height: 20)
-//            }
-//            .contentShape(Circle())
-            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+        HStack {
+            ZStack(alignment: .leading) {
                 RoundedRectangle(cornerRadius: 25)
                     .strokeBorder(Color.gray.opacity(0.6), lineWidth: 1)
                     .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).shadow(color: Color.black.opacity(0.1), radius: 5.0))
-                    .frame(width: 20, height: 20)
-            }
+                    .frame(width: 150, height: 150)
                 
+                Text("Go for a run")
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                    .padding(.all, 10.0)
+                    .frame(width: 85.0, height: 85.0)
+                
+                Text("🏃🏻‍♂️")
+                    .font(.system(size: 60))
+                    .padding(.leading, 85.0)
+                
+                Text("5 POINTS")
+                    .font(.system(size: 10))
+                    .padding(.bottom, 120.0)
+                    .padding(.leading, 90.0)
+                    .opacity(0.3)
+                
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    RoundedRectangle(cornerRadius: 25)
+                        .strokeBorder(Color.gray.opacity(0.6), lineWidth: 1)
+                        .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).shadow(color: Color.black.opacity(0.1), radius: 5.0))
+                        .frame(width: 20, height: 20)
+                        .offset(x:65, y:55)
+                }
+                    
+            }
+            
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 25)
+                    .strokeBorder(Color.gray.opacity(0.6), lineWidth: 1)
+                    .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).shadow(color: Color.black.opacity(0.1), radius: 5.0))
+                    .frame(width: 150, height: 150)
+                
+                Text("Go for a run")
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                    .padding(.all, 10.0)
+                    .frame(width: 85.0, height: 85.0)
+                
+                Text("🏃🏻‍♂️")
+                    .font(.system(size: 60))
+                    .padding(.leading, 85.0)
+                
+                Text("5 POINTS")
+                    .font(.system(size: 10))
+                    .padding(.bottom, 120.0)
+                    .padding(.leading, 90.0)
+                    .opacity(0.3)
+                
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    RoundedRectangle(cornerRadius: 25)
+                        .strokeBorder(Color.gray.opacity(0.6), lineWidth: 1)
+                        .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).shadow(color: Color.black.opacity(0.1), radius: 5.0))
+                        .frame(width: 20, height: 20)
+                        .offset(x:65, y:55)
+                }
+                    
+            }
+            
         }
     }
 }
